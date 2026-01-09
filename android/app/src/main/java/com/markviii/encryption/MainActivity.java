c250e0aa88ecf38b3bafa5e16a3e1807e3ab8a32
Proftvv <ozcanyilmazcelebi2016@gmail.com>
2026-01-09 15:07:44 +0300

    chore: Remove mobile/Android files and old documentation

--- android/app/src/main/java/com/markviii/encryption/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package com.markviii.encryption;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- fix_files.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Fixing files...
-
-del create_structure.js 2>nul
-ren create_structure_fixed.js create_structure.js
-
-echo Fixed!
-echo Now run: install.bat
-pause
